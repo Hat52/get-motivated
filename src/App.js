81d89335f7336467434a5de28c2eba5b9fbb93f3
HAT52 <tanolihamzaali@gmail.com>
2022-08-19 23:44:00 +0500

    global font-family

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Card from './components/cards'
+import './App.css'
 function App() {
   return (
     <div style={{display:"flex",justifyContent: 'center',alignItems: 'center',height:"100vh"}} className="App">
