5cb455d15cc901b03835f0429c59f435de5aeff8
Hat52 <tanolihamzaali@gmail.com>
2022-09-02 23:24:35 +0500

    feat: Share

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './index.css'
 import {BiComment} from 'react-icons/bi'
-import {AiOutlineLike} from 'react-icons/ai'
-import {AiOutlineDislike} from 'react-icons/ai'
+import {AiOutlineLike,AiOutlineDislike} from 'react-icons/ai'
+import {BsShare}from 'react-icons/bs'
 export default function Card ({index,author,quote}) {
     return(
         <div className='card-container' key={index}>
@@ -18,6 +18,7 @@ export default function Card ({index,author,quote}) {
                     <div className="Icon-text"><span>Comment</span> <BiComment fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
                     <div className="Icon-text"><span>Like</span> <AiOutlineLike fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
                     <div className="Icon-text"><span>Dislike</span> <AiOutlineDislike fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
+                    <div className="Icon-text"><span>Share</span> <BsShare fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
                 </div>
             </section>
         </div>
