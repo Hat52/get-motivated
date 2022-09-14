d704888ecf09e4ae39219cb628a3365faa24ca56
Hat52 <tanolihamzaali@gmail.com>
2022-09-15 00:31:05 +0500

    Feat:Remove actions on quote

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './index.css'
-import {BiComment} from 'react-icons/bi'
-import {AiOutlineLike,AiOutlineDislike} from 'react-icons/ai'
-import {BsShare}from 'react-icons/bs'
 export default function Card ({index,author,quote}) {
     return(
         <div className='card-container' key={index}>
@@ -13,14 +10,6 @@ export default function Card ({index,author,quote}) {
             <div className="author-name">
                 {author}
             </div>
-            <section className="comment-section-container">
-                <div className="comment-section">
-                    <div className="Icon-text" onClick={()=>{console.log("Comment clicked")}}><span>Comment</span> <BiComment fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
-                    <div className="Icon-text" onClick={()=>{console.log("Like clicked")}}><span>Like</span> <AiOutlineLike fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
-                    <div className="Icon-text" onClick={()=>{console.log("Dislike clicked")}}><span>Dislike</span> <AiOutlineDislike fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
-                    <div className="Icon-text" onClick={()=>{console.log("Share clicked")}}><span>Share</span> <BsShare fontSize="1.5em" color="#45CCDF" className="comment-icon" size="20"/></div>
-                </div>                   
-            </section>
             
         </div>
     )
